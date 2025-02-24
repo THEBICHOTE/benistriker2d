9fb7f6b8800e47b2781d2df14db2249915fb1120
THEBICHOTE <128328867+THEBICHOTE@users.noreply.github.com>
2025-02-24 12:48:47 -0400

    Update main.py
    integracion del movimiento

--- beni Striker/main.py
+++ beni Striker/main.py
@@ -1,15 +1,33 @@
 import pygame
 import constantes
-pygame.init ()
-ANCHO_VENTANA = 900
-Alto_VENTANA = 680
+from Jugador import Jugardor
+
+Jugardor= Jugardor(x=50,y=50)
+
+pygame.init()
+
 ventana= pygame.display.set_mode((constantes.ANCHO_VENTANA, constantes.ALTO_VENTANA))
+
 pygame.display.set_caption("Counter Striker 2D")
 run = True
-
-
 while run == True:
+ Jugardor.dibujar(ventana)
  for event in pygame.event.get():
+      
+     #para cerrar el juego 
      if event.type == pygame.QUIT:
-          run = False
+           run = False
+      
+if event.type == pygame.KEYDOWN:
+      if event .key == pygame.K_a:
+            print("Izquierda")
+      if event .key == pygame.K_d:
+            print("Derecha")
+      if event .key == pygame.K_w:
+            print("Arriba")
+      if event .key == pygame.K_s:
+            print("Abajo")
+
+pygame.display.update()
+
 pygame.quit()
