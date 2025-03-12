bd1c59fde01f8977495de7ff698d596bf6909c1c
THEBICHOTE <128328867+THEBICHOTE@users.noreply.github.com>
2025-03-11 23:27:10 -0400

    Update constantes.py

--- beni Striker/constantes.py
+++ beni Striker/constantes.py
@@ -1,2 +1,10 @@
+#tamaño de la ventana 
 ANCHO_VENTANA= 900
 ALTO_VENTANA= 680
+#coordenada jugador 
+COORDENADA_x= 50
+COORDENADA_y= 50
+#color del fondo
+COLOR_BG= (0,0,20)
+#velocidad de la bala
+BALA_VELOCIDAD = 10
