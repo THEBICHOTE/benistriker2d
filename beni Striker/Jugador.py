cc4abf07f2f778b74475cb671bcbfc55cda44875
THEBICHOTE <128328867+THEBICHOTE@users.noreply.github.com>
2025-03-11 23:26:43 -0400

    Update Jugador.py

--- beni Striker/Jugador.py
+++ beni Striker/Jugador.py
@@ -1,10 +1,14 @@
 import pygame
 
-class Jugardor():
-    def __init__ (self, x, y):
-        self.forma = pygame.Rect(0,0,40,40)
-        self.forma.center = (x,y)
+class Jugador:
+    def __init__(self, x, y):
+        self.forma = pygame.Rect(0, 0, 40, 40)
+        self.forma.center = (x, y)
+        self.velocidad = 5  # Se puede cambiar la velocidad fácilmente
+        self.color = (255, 255, 0)  # Color Amarillo
 
-    def dibujar(self, interfaz):
-        pygame.draw.rect(interfaz, (255, 255,0), self.forma)
-                 
+    def movimiento(self, delta_x, delta_y):
+        self.forma.move_ip(delta_x, delta_y)  # Método más eficiente para mover
+
+    def dibujar(self, ventana):
+        pygame.draw.rect(ventana, self.color, self.forma)
